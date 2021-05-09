3d109ae411647ee0007fe54114ec25a306b8f0d5
Chris Johnson <chrisjohnson1138@gmail.com>
2021-05-09 12:14:17 +0100

    Finished new book form. Started adding delete function

--- library.js
+++ library.js
@@ -7,10 +7,9 @@ function Book(title, author, pages, read){
     this.read = read
 }
 
-let theHobbit = new Book("The Hobbit", 'JRR Tolkien', 304, true);
-let greatGatsby = new Book('The Great Gatsby', 'F Scott Fitzgerald', 218, true);
-let catsCradle = new Book('Cats Cradle', 'Kurt Vonnegut', 304, false);
-
+let theHobbit = new Book("The Hobbit", 'JRR Tolkien', 304, 'Yes');
+let greatGatsby = new Book('The Great Gatsby', 'F Scott Fitzgerald', 218, 'Yes');
+let catsCradle = new Book('Cats Cradle', 'Kurt Vonnegut', 304, 'No');
 
 function addBookToLibrary(title,author,pages,read){
     let book = new Book(title,author,pages,read);
@@ -18,24 +17,26 @@ function addBookToLibrary(title,author,pages,read){
 }
 
 myLibrary.push(theHobbit, greatGatsby, catsCradle);
-addBookToLibrary("The Unbearable Lightness of Being", 'Milan Kundera', 393, true);
+addBookToLibrary("The Unbearable Lightness of Being", 'Milan Kundera', 393, 'Yes');
+addBookToLibrary("Intimacy", 'Hanif Kureshi', 220, 'Yes');
+
 
 const cardContainer = document.getElementById('card-container');
+
+function generateCard(num, book){
+    let bookDiv = document.createElement('div');
+        bookDiv.id = "book" + num;
+        bookDiv.classList.add('book-card');
+        bookDiv.innerHTML = "Title: " + book.title + "</br> Author: " + 
+        book.author + "</br> Page Count: " + book.pages + "</br> Have you read it? " +
+        book.read + '</br> <span class = "book-delete">Delete</span>';
+        cardContainer.appendChild(bookDiv);
+}
 
 function displayBooks(){
     let n = 0;
     myLibrary.forEach(function(book){
-        let readIt = 'Yes';
-        if(!book.read){
-            readIt = 'No';
-        }
-        let bookDiv = document.createElement('div');
-        bookDiv.id = "book" + n;
-        bookDiv.classList.add('book-card');
-        bookDiv.innerHTML = "Title: " + book.title + "</br> Author: " + 
-        book.author + "</br> Page Count: " + book.pages + "</br> Have you read it? " +
-        readIt;
-        cardContainer.appendChild(bookDiv);
+        generateCard(n, book);
         n++;
     })
 }
@@ -45,6 +46,10 @@ displayBooks();
 const newBookBtn = document.getElementById('new-book-btn');
 const formContainer = document.getElementById('form-container');
 const formX = document.getElementById('x');
+
+function removeCards(){
+    bookCards.forEach(book => book.remove());
+}
 
 function showForm(){
     formContainer.style.display = 'flex';
@@ -58,3 +63,36 @@ newBookBtn.addEventListener('click', showForm);
 
 formX.addEventListener('click', hideForm);
 
+const submitBtn = document.getElementById('submit');
+
+submitBtn.addEventListener('click', submitForm);
+let formTitle = document.getElementById('form-title');
+let formAuthor = document.getElementById('form-author');
+let formPages = document.getElementById('form-pages');
+let formRead = document.getElementById('form-read');
+let formFields = document.querySelectorAll('input');
+
+function clearForm(){
+    formFields.forEach(field => 
+       field.value = '' )
+}
+
+function submitForm(){
+    console.log(formRead.value);
+    let readTick = 'No';
+    if (!formRead.value){
+        readTick = "Yes"
+    }
+    addBookToLibrary(formTitle.value, formAuthor.value, formPages.value, readTick);
+    generateCard(myLibrary.length, myLibrary[(myLibrary.length)-1]);
+    clearForm();
+    hideForm();
+}
+
+let deleteButtons = document.querySelectorAll('.book-delete');
+deleteButtons.forEach(deleteButton => addEventListener('click', deleteBook));
+
+function deleteBook(e){
+    console.log(myLibrary);
+    console.log(e);
+}
